0c4e8a61f9266bde93261a22c4886829cf3c68da
Claudio Figueroa <claudio.figueroa.urtubia@gmail.com>
2025-06-18 00:10:38 -0400

    Prueba de CI

--- sum.js
+++ sum.js
@@ -1,6 +1,7 @@
 function sum(a, b) {
     //return a + b;
-    return (2 * a) + (2 * b); // O 2 * (a + b);
+    //return (2 * a) + (2 * b); // O 2 * (a + b);
+    return b + a;
 }
 
 module.exports = sum;
